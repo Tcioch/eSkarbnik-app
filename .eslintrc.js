e8cfbebd29684231e1c96a2a08328b8a58d3194a
Tcioch <35658729+Tcioch@users.noreply.github.com>
2020-09-13 15:24:19 +0200

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,29 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    "plugin:vue/recommended",
+    "@vue/standard",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: 0,
+    semi: 0,
+    "space-before-function-paren": 0,
+    "vue/html-self-closing": 0,
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: 4,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
   }
-}
+};
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import i18n from "./i18n";
+import VueCompositionApi from "@vue/composition-api";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi);
 
 new Vue({
   router,
   store,
+  vuetify,
+  i18n,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
